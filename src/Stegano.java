7410e91e48f7caf869195fee2da396584502e2e5
Валентин <wippi2010@rambler.ru>
2017-07-04 17:19:40 +0300

    Create stegano

--- src/Stegano.java
+++ src/Stegano.java
@@ -1,5 +1,19 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Валентин on 04.07.2017.
  */
 public class Stegano {
+    public static void main(String[] args) {
+        File file = new File("src/java.jpg");
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
